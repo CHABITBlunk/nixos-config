aec58fe5cf6002bb6202025cd9d6ee95de368ceb
chabitblunk <cblunk097@gmail.com>
2024-08-13 15:04:41 -0600

    about to port to nixos

--- modules/home-manager/gtk/default.nix
+++ modules/home-manager/gtk/default.nix
@@ -1,5 +1,24 @@
 { config, pkgs, ... }:
 {
-  programs.nwg-look.enable = true;
-  programs.lxappearance.enable = true;
+  gtk  = {
+    enable = true;
+    theme = {
+      name = "sweet-dark";
+      package = pkgs.sweet;
+    };
+    iconTheme = {
+      name = "candy-icons";
+      package = pkgs.candy-icons;
+    };
+    cursorTheme = {
+      name = "Bibata-Ice";
+      package = pkgs.bibata-cursors;
+      size = 16;
+    };
+    font = {
+      name = "Ubuntu Nerd Font";
+      package = with pkgs; [ (pkgs.nerdfonts.override { fonts = [ "Ubuntu" ]; }) ];
+      size = "12";
+    };
+  }
 }
